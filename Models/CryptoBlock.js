671888767a7a377fbe40095bbe5f227710cc9c7e
Dinuda Yaggahavita <ydinuda@gmail.com>
2021-02-20 18:06:33 +0530

    create cryptoBlock class

--- Models/CryptoBlock.js
+++ Models/CryptoBlock.js
@@ -1,2 +1,14 @@
 const SHA256 = require('crypto-js/sha256')
 
+class CryptoBlock {
+    constructor(index, timestamp, data, precedingHash = " ") {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.precedingHash = precedingHash;
+        this.hash = this.computeHash();
+    }
+    computeHash() {
+        return SHA256(this.index + this.precedingHash + this.timestamp + JSON.stringif(this.data)).toString();
+    }
+}

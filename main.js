763b61a3ed1bd583c18b16081ba368616a839afe
Dinuda Yaggahavita <ydinuda@gmail.com>
2021-02-21 14:57:24 +0530

    edit main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const CryptoBlock = require('./Models/CryptoBlock');
 
 let smashingCoin = new CryptoBlockChain();
 smashingCoin.addNewBlock(new CryptoBlock(1, "2/20/2021", { sender: "Dinuda Yaggahavita", recipient: "Lehan Gajanayake", quantity: 200 }))
-smashingCoin.addNewBlock(new CryptoBlock(2, "2/21/2021", { sender: "Lehan Gajanayake", recipient: "", quantity: 150 }))
-smashingCoin.addNewBlock(new CryptoBlock(2, "2/21/2021", { sender: "Uvindu Pathirana", recipient: "Tarith Jayasooriya", quantity: 100 }))
+smashingCoin.addNewBlock(new CryptoBlock(2, "2/21/2021", { sender: "Lehan Gajanayake", recipient: "Uvindu Pathirana", quantity: 150 }))
+smashingCoin.addNewBlock(new CryptoBlock(2, "2/22/2021", { sender: "Uvindu Pathirana", recipient: "Tarith Jayasooriya", quantity: 100 }))
 
 console.log(JSON.stringify(smashingCoin, null, 4));

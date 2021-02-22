2a08d84ef73f0f0074ee5363bfaa08426cd7b4f9
Dinuda Yaggahavita <ydinuda@gmail.com>
2021-02-22 22:11:27 +0530

    add date time to starting block

--- Models/CryptoBlockChain.js
+++ Models/CryptoBlockChain.js
@@ -4,6 +4,9 @@ const date = require('date-and-time');
 // Import CryptoBlock
 const CryptoBlock = require('./CryptoBlock');
 
+// Declarations
+const now = new Date();
+
 class CryptoBlockChain {
     constructor() {
         // array of blocks
@@ -11,7 +14,7 @@ class CryptoBlockChain {
         this.difficulty = 4;
     }
     startGenesisBlock() {
-        return new CryptoBlock(0, "01/01/2020", "Initial Block in the Chain", "0");
+        return new CryptoBlock(0, date.format(now, 'YYYY/MM/DD HH:mm:ss'), "Initial Block in the Chain", "0");
     }
 
     obtainLatestBlock() {

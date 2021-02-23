0596f89272bbb2d848bcb6ef31e111b6217480ad
Dinuda Yaggahavita <ydinuda@gmail.com>
2021-02-23 09:44:09 +0530

    add date to main

--- README.md
+++ README.md
@@ -9,11 +9,10 @@ Simple Cryptocurrency Blockchain In Node.js
 ## Install crypto
 
     npm install crypto-js
-    
+
 ## Install Date-And-Time
 
-npm install date-and-time --save
-
+    npm install date-and-time --save
 
 ## Run the application
 
--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 // Packages
 const fs = require('fs')
 const chalk = require('chalk')
+const date = require('date-and-time');
 
 // Files
 const CryptoBlockChain = require('./Models/CryptoBlockChain');
 const CryptoBlock = require('./Models/CryptoBlock');
+
+// Declarations
+const now = new Date();
 
 const loadAccountBalance = () => {
     try {
         const dataBuffer = fs.readFileSync('Accounts/Dinuda.json')
         const dataJSON = dataBuffer.toString()
         return JSON.parse(dataJSON)
+
     } catch (e) {
         return []
     }
@@ -18,8 +23,6 @@ const loadAccountBalance = () => {
 
 
 let smashingCoin = new CryptoBlockChain();
-smashingCoin.addNewBlock(new CryptoBlock(1, "2/20/2021", { sender: "Dinuda Yaggahavita", recipient: "Lehan Gajanayake", quantity: 200 }))
-smashingCoin.addNewBlock(new CryptoBlock(2, "2/21/2021", { sender: "Lehan Gajanayake", recipient: "Uvindu Pathirana", quantity: 150 }))
-smashingCoin.addNewBlock(new CryptoBlock(2, "2/22/2021", { sender: "Uvindu Pathirana", recipient: "Tarith Jayasooriya", quantity: 100 }))
+smashingCoin.addNewBlock(new CryptoBlock(1, date.format(now, 'YYYY/MM/DD HH:mm:ss'), { sender: "Dinuda Yaggahavita", recipient: "Lehan Gajanayake", quantity: 200 }))
 
 console.log(JSON.stringify(smashingCoin, null, 4));
